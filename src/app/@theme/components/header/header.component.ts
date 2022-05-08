74bd207a900e3f4ec1ac30af9d32c7c19fdee0a5
WassimSlim21 <Wassimslim18@gmail.com>
2022-05-09 00:11:31 +0100

    adding all reports pages

--- src/app/@theme/components/header/header.component.ts
+++ src/app/@theme/components/header/header.component.ts
@@ -49,11 +49,11 @@ export class HeaderComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    this.currentTheme = "this.themeService.currentTheme";
-
+   this.currentTheme = this.themeService.currentTheme;
+    this.themeService.changeTheme("dark");
     this.userService.getUsers()
       .pipe(takeUntil(this.destroy$))
-      .subscribe((users: any) => this.user = users.nick);
+      .subscribe((users: any) => this.user = users.wassim);
 
     const { xl } = this.breakpointService.getBreakpointsMap();
     this.themeService.onMediaQueryChange()
