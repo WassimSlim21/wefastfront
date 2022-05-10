b03c200e75ee06495c05d78a78165e4a2d0eca8c
WassimSlim21 <Wassimslim18@gmail.com>
2022-05-10 09:46:13 +0100

    temp

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -65,72 +65,70 @@ const routes: Routes = [
       {
         path: "prediction",
         loadChildren: () => import('./prediction/prediction.module')
-        .then(m => m.PredictionModule),      }
-
-      // },
-      // {
-      //   path: 'drivers',
-      //   component: DriversComponent,
-      // },
-      // {
-      //   path: 'layout',
-      //   loadChildren: () => import('./layout/layout.module')
-      //     .then(m => m.LayoutModule),
-      // },
-      // {
-      //   path: 'forms',
-      //   loadChildren: () => import('./forms/forms.module')
-      //     .then(m => m.FormsModule),
-      // },
-      // {
-      //   path: 'ui-features',
-      //   loadChildren: () => import('./ui-features/ui-features.module')
-      //     .then(m => m.UiFeaturesModule),
-      // },
-      // {
-      //   path: 'modal-overlays',
-      //   loadChildren: () => import('./modal-overlays/modal-overlays.module')
-      //     .then(m => m.ModalOverlaysModule),
-      // },
-      // {
-      //   path: 'extra-components',
-      //   loadChildren: () => import('./extra-components/extra-components.module')
-      //     .then(m => m.ExtraComponentsModule),
-      // },
-      // {
-      //   path: 'maps',
-      //   loadChildren: () => import('./maps/maps.module')
-      //     .then(m => m.MapsModule),
-      // },
-      // {
-      //   path: 'charts',
-      //   loadChildren: () => import('./charts/charts.module')
-      //     .then(m => m.ChartsModule),
-      // },
-      // {
-      //   path: 'editors',
-      //   loadChildren: () => import('./editors/editors.module')
-      //     .then(m => m.EditorsModule),
-      // },
-      // {
-      //   path: 'tables',
-      //   loadChildren: () => import('./tables/tables.module')
-      //     .then(m => m.TablesModule),
-      // },
-      // {
-      //   path: 'miscellaneous',
-      //   loadChildren: () => import('./miscellaneous/miscellaneous.module')
-      //     .then(m => m.MiscellaneousModule),
-      // },
-      // {
-      //   path: '',
-      //   redirectTo: 'dashboard',
-      //   pathMatch: 'full',
-      // },
-      // {
-      //   path: '**',
-      //   component: NotFoundComponent,
-      // },
+        .then(m => m.PredictionModule),      },
+      {
+        path: 'drivers',
+        component: DriversComponent,
+      },
+      {
+        path: 'layout',
+        loadChildren: () => import('./layout/layout.module')
+          .then(m => m.LayoutModule),
+      },
+      {
+        path: 'forms',
+        loadChildren: () => import('./forms/forms.module')
+          .then(m => m.FormsModule),
+      },
+      {
+        path: 'ui-features',
+        loadChildren: () => import('./ui-features/ui-features.module')
+          .then(m => m.UiFeaturesModule),
+      },
+      {
+        path: 'modal-overlays',
+        loadChildren: () => import('./modal-overlays/modal-overlays.module')
+          .then(m => m.ModalOverlaysModule),
+      },
+      {
+        path: 'extra-components',
+        loadChildren: () => import('./extra-components/extra-components.module')
+          .then(m => m.ExtraComponentsModule),
+      },
+      {
+        path: 'maps',
+        loadChildren: () => import('./maps/maps.module')
+          .then(m => m.MapsModule),
+      },
+      {
+        path: 'charts',
+        loadChildren: () => import('./charts/charts.module')
+          .then(m => m.ChartsModule),
+      },
+      {
+        path: 'editors',
+        loadChildren: () => import('./editors/editors.module')
+          .then(m => m.EditorsModule),
+      },
+      {
+        path: 'tables',
+        loadChildren: () => import('./tables/tables.module')
+          .then(m => m.TablesModule),
+      },
+      {
+        path: 'miscellaneous',
+        loadChildren: () => import('./miscellaneous/miscellaneous.module')
+          .then(m => m.MiscellaneousModule),
+      },
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+      },
+      {
+        path: '**',
+        component: NotFoundComponent,
+      }
     ],
   },
 ];
